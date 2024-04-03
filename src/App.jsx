593dd579684f6698e669124174eef839cccc66eb
rushikesh2020 <31483483+rushikesh2020@users.noreply.github.com>
2024-04-04 00:26:49 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './assets/components/Navbar.jsx';
+import Main from './assets/components/Main.jsx';
+
+export default function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
